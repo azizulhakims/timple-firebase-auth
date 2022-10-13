36e163c067a8798aa2da37c9f626cb0802c3a8b5
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-13 16:09:04 +0600

    add firebase init file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import app from './firebase/firebase.init';
+
+
+const auth = getAuth(app);
 
 function App() {
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+
+      .catch(error => {
+        console.log('error', error);
+      })
+  }
+
+
   return (
     <div className="App">
-
+      <button onClick={handleGoogleSignIn}>Google Sing In</button>
     </div>
   );
 }
